2ff39267f973fcc7e02ade9040ebecc2b70cc159
eslamkenan1 <eslamkhaledkenan@gmail.com>
2023-12-14 05:49:06 +0200

    favorite places app

---
+++ favorite_places/lib/widgets/places_list.dart
@@ -0,0 +1,56 @@
+import 'package:favorite_places/models/place_model.dart';
+import 'package:favorite_places/screens/place_details.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          "no places added",
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            radius: 26,
+            backgroundImage: FileImage(places[index].image),
+          ),
+          title: Text(
+            places[index].title,
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          subtitle: Text(
+            places[index].location.address,
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => PlaceDetailsScreen(
+                place: places[index],
+              ),
+            ));
+          },
+        );
+      },
+    );
+  }
+}
